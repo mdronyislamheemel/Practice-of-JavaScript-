628086dd89f9da6dd4cbe8523cfc2dd0d7674736
Md Rony Islam <mdronyislam149@gmail.com>
2025-02-01 12:39:59 +0600

    Initial commit

---
+++ First.js
@@ -0,0 +1,5 @@
+// just print out
+
+console.log("My first code at JS");
+console.log("I love JavaScript");
+
---
+++ variable_and_data_types.js
@@ -0,0 +1,48 @@
+//variable declaration
+age =25;
+age = age + 1;
+price = 99.99;
+Name = "Rony islam";
+x = null;
+y = undefined;
+isFollow = true;
+//or this can be false
+
+console.log(age);
+console.log(price);
+console.log(Name);
+console.log(x);
+console.log(y);
+console.log(isFollow);
+
+// there are 3 types of variable declaration in javascript
+// 1. var: can be re-declared and updated. A global scope variable.
+// 2. let: can be updated but not re-declared. A block scope variable.
+// 3. const: can not be updated and re-declared. A block scope variable.
+
+var a = 10;
+var a = 20;
+var a = 30;
+console.log(a);
+// tobe 2015 er por theke var var use kora hoi na.
+
+let b = 10;
+//  abar let b = 20; hobe na karon same variable duibar declare kora jabe na
+b = 20;
+console.log(b);
+
+const c = 10;
+const d = 20;
+// akhane sudhu c = 30; ba const c = 30; dile error asbe karon const variale re-declare kora jabe na
+
+// data types
+// 1. primitive data types
+// number, string, boolean, null, undefined, symbol
+// 2. non-primitive data types
+// object, array, function
+
+const num = 10;
+const str = "I love JS";
+const isTrue = true;
+const rony = null;
+let x;
