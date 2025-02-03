eb32420336e33cf67e83746a561ef23d377c6ab7
Md Rony Islam <mdronyislam149@gmail.com>
2025-02-04 01:05:44 +0600

    some changes are occured

---
+++ loops_&_strings.js
@@ -0,0 +1,12 @@
+// for in loops
+let student1 = {
+    name: "Rony Islam",
+    age: 26,
+    cgpa: 3.13,
+    isPass: true,
+};
+
+for(let i in student1){
+    console.log("key:",i , "value", student1[i])
+}
+
---
+++ user_input_practice.js
@@ -0,0 +1,10 @@
+// browser e akta popup message dekhanor jonno use kora hoi alert()
+// alert("Hello");
+// r user er kache akta input soho popup message ase jodi promt use kora hoi
+let num1 = prompt("Enter a number");
+if(num1 % 5 === 0){
+    console.log(num1," is multiple of 5");
+}
+else {
+    console.log(num1," num is not multiple of 5");
+}
